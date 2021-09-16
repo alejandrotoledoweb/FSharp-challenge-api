f7974dce825231cb98511d63c4215e58aa7ed892
alejandro toledo <atoledofr@gmail.com>
2021-09-16 12:12:05 -0500

    add endpoints to the Route and add the get request to the webApp in server file

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -2,12 +2,53 @@ module Server
 
 open Giraffe
 open Saturn
+open FSharp.Control.Tasks
 
 open Shared
+
+type Storage() =
+    let blotters = ResizeArray<_>()
+    let markets = ResizeArray<_>()
+
+    member __.GetBlotters() = List.ofSeq blotters
+    
+    member __.GetMarkets() = List.ofSeq markets
+
+    member __.AddBlotter(data : Blotter) =
+        blotters.Add data
+        Ok()
+
+    member __.AddMarket(dataMarket : Market) =
+        markets.Add dataMarket
+        Ok()
+
+let storage = Storage()        
+
+let loadBlottersInfo =
+    [ { Id = System.Guid();
+        DateTime = System.DateTime()
+        Price = 1.4528;
+        Quantity = 1500;
+        Pair = "GBP/USD" } ]
+
+let loadMarketsInfo =
+    [ { Id = System.Guid();
+        Provider = "CBOE";
+        Pair = "GBP/USD";
+        Price = 1.554;
+        Time = System.DateTime() } ]
+
+// let getBlotters next ctx =
+//     json (loadBlottersInfo()) next ctx
+
+// let getMarkets next ctx =
+//     json (loadMarketsInfo()) next ctx
 
 let webApp =
     router {
         get Route.hello (text "Hello from SAFE!")
+        get Route.blotters (json (loadBlottersInfo))
+        get Route.markets (json (loadMarketsInfo))
     }
 
 let app =
