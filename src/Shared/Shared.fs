106fc52b829147a14664fa0488768fbc32b81e5c
alejandro toledo <atoledofr@gmail.com>
2021-09-16 10:44:50 -0500

    add type for blotter and market

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -3,3 +3,24 @@ open System
 
 module Route =
     let hello = "/api/hello"
+    let blotter = "/api/blotter"
+
+type Blotter = { Id: Guid; DateTime: DateTime; Price: float; Quantity: int; Pair: string }
+
+type Market = { Id: Guid; Provider: string; Pair: string; Price: float; Time: DateTime}
+
+module Blotter =
+    let create dateTime price quantity pair =
+        { Id = Guid.NewGuid()
+          DateTime = dateTime
+          Price = price
+          Quantity = quantity
+          Pair = pair }
+
+module Market =
+    let create provider pair price time =
+        { Id = Guid.NewGuid()
+          Provider = provider
+          Pair = pair
+          Price = price
+          Time = time }
